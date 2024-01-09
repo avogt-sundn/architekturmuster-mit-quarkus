e0a88c122541042d87b330d1abd3e594648d185b
Armin Vogt <armin.vogt@sn-invent.de>
2024-01-09 23:47:37 +0100

    load test

--- src/main/grafana/scripts/script.js
+++ src/main/grafana/scripts/script.js
@@ -3,7 +3,7 @@ import http from 'k6/http';
 
 const isNumeric = (value) => /^\d+$/.test(value);
 
-const default_vus = 5;
+const default_vus = 50;
 
 const target_vus_env = `${__ENV.TARGET_VUS}`;
 const target_vus = isNumeric(target_vus_env) ? Number(target_vus_env) : default_vus;
@@ -13,8 +13,14 @@ export let options = {
       // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 5s
       { duration: "5s", target: target_vus },
 
-      // Stay at rest on TARGET_VUS VUs for 10s
-      { duration: "10s", target: target_vus },
+      // Stay at rest on TARGET_VUS VUs for 50s
+      { duration: "30s", target: target_vus },
+
+      // Stay at rest on TARGET_VUS VUs for 50s
+      { duration: "30s", target: target_vus * 10 },
+
+      // Stay at rest on TARGET_VUS VUs for 50s
+        { duration: "10s", target: target_vus },
 
       // Ramp-down from TARGET_VUS to 0 VUs for 5s
       { duration: "5s", target: 0 }
@@ -22,7 +28,7 @@ export let options = {
 };
 
 export default function () {
-  const response = http.get("https://swapi.dev/api/people/30/", {headers: {Accepts: "application/json"}});
+  const response = http.get("http://host.docker.internal:8080/organizations?_pagesize=100&beschreibung=GetPaginated", {headers: {Accepts: "application/json"}});
   check(response, { "status is 200": (r) => r.status === 200 });
   sleep(.300);
 };
