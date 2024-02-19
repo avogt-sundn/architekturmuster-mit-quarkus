bae0386895575dbbe36e6fd65e0efb6f4b28041e
Armin Vogt <armin.vogt@sn-invent.de>
2024-02-19 23:22:59 +0100

    step

--- openfga/src/test/java/quarkitecture/OpenFGATest.java
+++ openfga/src/test/java/quarkitecture/OpenFGATest.java
@@ -1,19 +1,27 @@
 package quarkitecture;
 
-import static java.time.Duration.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.quarkiverse.openfga.client.AuthorizationModelClient;
 import io.quarkiverse.openfga.client.OpenFGAClient;
 import io.quarkiverse.openfga.client.StoreClient;
 import io.quarkiverse.openfga.client.model.Store;
-import io.quarkiverse.openfga.client.model.Tuple;
+import io.quarkiverse.openfga.client.model.TupleKey;
+import io.quarkiverse.openfga.client.model.TypeDefinition;
+import io.quarkiverse.openfga.client.model.TypeDefinitions;
 import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -31,7 +39,6 @@ class OpenFGATest {
         this.authModelClient = authModelClient;
     }
 
-
     @SuppressWarnings("null")
     @Test
     void _StoresCreateDelete() {
@@ -48,13 +55,58 @@ class OpenFGATest {
         Log.info("listAllStores:" + listAllStores);
         assertThat(listAllStores).isNotNull();
         // at least one store matches our name
-        client.listAllStores().await().indefinitely().stream().anyMatch(s -> s.getName().equals(name));
+        assertTrue(client.listAllStores().await().indefinitely().stream().anyMatch(s -> s.getName().equals(name)));
 
         // delete store
-        storeClient.delete().await().atMost(ofSeconds(10));
+        delete(storeId, storeClient);
+    }
 
+    @SuppressWarnings("null")
+    @Test
+    void _Run() throws IOException {
         client.listAllStores().await().indefinitely().forEach(s -> {
-            if (s.getName().equals(name) || s.getId().equals(storeId)) {
+
+            if (s.getName().equals("test")) {
+                delete(s.getId(), client.store(s.getId()));
+            }
+        });
+        // create store
+        String storeId = client.createStore("test").await().indefinitely().getId();
+        // access store via store ID
+        StoreClient storeClient = client.store(storeId);
+        List<TypeDefinition> td = new ArrayList<>();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        // read file from classpath
+        String fileContents = Files.readString(Path.of("src/test/resources/auth-model.json"));
+        // byte[] file =
+        // this.getClass().getResourceAsStream("auth-model.json").readAllBytes();
+
+        assertThat(fileContents).isNotNull();
+        Log.info("file:" + fileContents);
+        TypeDefinitions modelFromFile = objectMapper.readValue(
+                fileContents,
+                TypeDefinitions.class);
+        // create a tuple
+        String modelId = storeClient.authorizationModels().create(modelFromFile.getTypeDefinitions()).await()
+                .indefinitely();
+        AuthorizationModelClient model = storeClient.authorizationModels().model(modelId);
+
+        model.write(
+                TupleKey.of("thing:1",
+                        "owner",
+                        "user:me"))
+                .await().indefinitely();
+
+        Boolean check = model.check(TupleKey.of("test", "test", "test"), null).await().indefinitely();
+        assertThat(check).isTrue();
+        // delete store
+        delete(storeId, storeClient);
+    }
+
+    private void delete(String storeId, StoreClient storeClient) {
+        storeClient.delete().await().indefinitely();
+        client.listAllStores().await().indefinitely().forEach(s -> {
+            if (s.getId().equals(storeId)) {
                 Assertions.fail("Store not deleted");
             }
         });
