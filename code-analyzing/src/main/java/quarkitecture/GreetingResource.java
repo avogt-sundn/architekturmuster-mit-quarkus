3949577a8f781d13bf712c4cc901866861a4629a
Armin Vogt <armin.vogt@sn-invent.de>
2024-02-21 18:28:05 +0100

    format code utf8

--- code-analyzing/src/main/java/quarkitecture/GreetingResource.java
+++ code-analyzing/src/main/java/quarkitecture/GreetingResource.java
@@ -2,15 +2,17 @@ package quarkitecture;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/items")
 public class GreetingResource {
 
 	@GET
+	@Path("{id}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String hello() {
+	public String getItem(@PathParam("id") String id) {
 
 		return "Hello from RESTEasy Reactive";
 	}
