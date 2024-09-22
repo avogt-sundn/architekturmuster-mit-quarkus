10a3a4635fd8c3a0c51efdb617127981a0b25504
avogt-sundn <avogt@s-und-n.de>
2024-09-22 09:19:20 +0000

    remove zweiterEintrag

--- liquibase/src/main/java/quarkitecture/liquibase/KatalogEntity.java
+++ liquibase/src/main/java/quarkitecture/liquibase/KatalogEntity.java
@@ -1,6 +1,7 @@
 package quarkitecture.liquibase;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,5 +25,4 @@ public class KatalogEntity extends PanacheEntityBase {
     Long id;
 
     String eintrag;
-    String zweiterEintrag;
 }
