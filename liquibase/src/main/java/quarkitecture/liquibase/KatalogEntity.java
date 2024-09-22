a807de80d010368d76b505fc58bc66630f37f49f
avogt-sundn <avogt@s-und-n.de>
2024-09-22 11:39:32 +0200

    go

--- liquibase/src/main/java/quarkitecture/liquibase/KatalogEntity.java
+++ liquibase/src/main/java/quarkitecture/liquibase/KatalogEntity.java
@@ -1,7 +1,6 @@
 package quarkitecture.liquibase;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
