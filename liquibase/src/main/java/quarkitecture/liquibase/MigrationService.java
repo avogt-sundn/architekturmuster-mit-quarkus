746f197af9b6e0e432213cbd0533ac88b2764d82
avogt-sundn <avogt@s-und-n.de>
2024-09-25 15:43:20 +0000

    fixes

--- liquibase/src/main/java/quarkitecture/liquibase/MigrationService.java
+++ liquibase/src/main/java/quarkitecture/liquibase/MigrationService.java
@@ -1,15 +1,6 @@
 package quarkitecture.liquibase;
 
-import java.util.List;
-
-import io.quarkus.liquibase.LiquibaseDataSource;
-import io.quarkus.liquibase.LiquibaseFactory;
-import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import liquibase.Liquibase;
-import liquibase.changelog.ChangeSetStatus;
-import liquibase.exception.LiquibaseException;
 
 @ApplicationScoped
 public class MigrationService {
@@ -19,15 +10,16 @@ public class MigrationService {
 
     // @Inject
     // public MigrationService(LiquibaseFactory liquibaseFactory) {
-    //     this.liquibaseFactory = liquibaseFactory;
+    // this.liquibaseFactory = liquibaseFactory;
     // }
 
     // public void checkMigration() throws LiquibaseException {
-    //     // Get the list of liquibase change set statuses
-    //     try (Liquibase liquibase = liquibaseFactory.createLiquibase()) {
-    //         List<ChangeSetStatus> status = liquibase.getChangeSetStatuses(liquibaseFactory.createContexts(),
-    //                 liquibaseFactory.createLabels());
-    //         Log.info("Liquibase status: " + status);
-    //     }
+    // // Get the list of liquibase change set statuses
+    // try (Liquibase liquibase = liquibaseFactory.createLiquibase()) {
+    // List<ChangeSetStatus> status =
+    // liquibase.getChangeSetStatuses(liquibaseFactory.createContexts(),
+    // liquibaseFactory.createLabels());
+    // Log.info("Liquibase status: " + status);
+    // }
     // }
 }
