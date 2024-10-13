74efd4dded230dba1f7432c2acd0fd1e4da334a9
avogt-sundn <avogt@s-und-n.de>
2024-10-13 09:03:33 +0000

    rest example for metrics

---
+++ load-testing/src/main/java/organisationen/attribute/ResourceWithMetrics.java
@@ -0,0 +1,81 @@
+package organisationen.attribute;
+
+import java.net.URI;
+import java.util.List;
+
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.quarkus.logging.Log;
+import io.smallrye.common.annotation.RunOnVirtualThread;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.json.JsonObject;
+import jakarta.json.bind.Jsonb;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ */
+@Path("resourcewithmetrics")
+@ApplicationScoped
+public class ResourceWithMetrics {
+
+    Jsonb jsonb;
+    private final LongCounter counter;
+
+    public ResourceWithMetrics(Jsonb jsonb, Meter meter) {
+
+        this.jsonb = jsonb;
+        counter = meter.counterBuilder("hello-metrics")
+                .setDescription("hello-metrics")
+                .setUnit("invocations")
+                .build();
+    }
+
+    @POST
+    @Transactional
+    public Response saveObjectFromJson(JsonObject body) {
+
+        AttributeEntity attributeEntity = new AttributeEntity();
+        Log.info(body);
+        attributeEntity.jsonString = body.toString();
+        attributeEntity.persist();
+        Long id = attributeEntity.id;
+
+        return Response.created(URI.create("/jsonresource/" + id)).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response load(@PathParam("id") Long id) {
+        counter.add(1);
+        AttributeEntity byId = AttributeEntity.findById(id);
+        return Response.ok(jsonb.fromJson(byId.jsonString, JsonObject.class)).build();
+    }
+
+    @GET
+    @Path("virtual")
+    @RunOnVirtualThread
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllVirtual() {
+        counter.add(1);
+        List<AttributeEntity> list = AttributeEntity.findAll().list();
+        return Response.ok(list).build();
+    }
+
+    @GET
+    @Path("classic")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllClassic() {
+        counter.add(1);
+        List<AttributeEntity> list = AttributeEntity.findAll().list();
+        return Response.ok(list).build();
+    }
+
+}
---
+++ load-testing/src/test/java/organisationen/attribute/ResourceWithMetricsTest.java
@@ -0,0 +1,75 @@
+package organisationen.attribute;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.http.HttpStatus;
+import org.jboss.logging.MDC;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+/**
+ *
+ */
+@QuarkusTest
+@TestHTTPEndpoint(ResourceWithMetrics.class)
+class ResourceWithMetricsTest {
+
+    static final Pattern locationHeaderMatchingPattern = Pattern.compile("http://.+/(\\d+)");
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @AfterEach
+    void After() {
+        MDC.clear();
+    }
+
+    @Test
+    void SaveObjectFromJson() {
+        final String randomUUID = UUID.randomUUID().toString();
+        final String location = given().with().contentType(ContentType.JSON).body("""
+                {
+                    "name": "armin",
+                    "surname": "vogt",
+                    "uuid": "%s"
+                }
+                """.formatted(randomUUID)).post()
+                .then().log().all()
+                .statusCode(equalTo(HttpStatus.SC_CREATED))
+                .header("Location", matchesPattern(locationHeaderMatchingPattern))
+                .and().extract().header("Location");
+
+        MDC.put("location", location);
+        Matcher matcher = locationHeaderMatchingPattern.matcher(location);
+        assertTrue(matcher.matches());
+        String group = matcher.group(1);
+        MDC.put("group", group);
+
+        Long id = Long.valueOf(group);
+        given().with().contentType(ContentType.JSON).pathParam("id", id).when().get("{id}").then()
+                .statusCode(equalTo(HttpStatus.SC_OK)).and().body("uuid", equalTo(randomUUID));
+
+    }
+
+    @Test
+    void SearchJsonObject() {
+        given().with()
+                .param("surname", "vogt")
+                .when().get("virtual").then().assertThat()
+                .statusCode(equalTo(HttpStatus.SC_OK));
+    }
+}
