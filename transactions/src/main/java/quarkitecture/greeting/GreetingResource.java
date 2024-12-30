eed6c4041fb3db040c78d8c23274e3d46deb99e2
avogt-sundn <avogt@s-und-n.de>
2024-12-30 18:16:14 +0000

    test shows XA works on constraint violation

--- transactions/src/main/java/quarkitecture/greeting/GreetingResource.java
+++ transactions/src/main/java/quarkitecture/greeting/GreetingResource.java
@@ -1,9 +1,11 @@
 package quarkitecture.greeting;
 
 import org.jboss.resteasy.reactive.ResponseStatus;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import jakarta.transaction.TransactionScoped;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
@@ -11,6 +13,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import quarkitecture.logentry.LogEntry;
 
 @Path("/hello")
@@ -22,6 +25,9 @@ public class GreetingResource {
         return "Hello from Quarkus REST";
     }
 
+    @TransactionScoped
+    String scoped;
+
     /**
      *
      * @param name
@@ -32,16 +38,42 @@ public class GreetingResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
-    public Greeting postHello(JsonNode name) {
+    public Greeting postHello(JsonNode json) {
 
-        Greeting greeting = new Greeting();
-        greeting.name = name.get("name").asText();
-        greeting.persist();
-
+        final String name = json.get("name").asText();
+        scoped = name;
         LogEntry logEntry = new LogEntry();
-        logEntry.message = "Received name: " + name;
+        logEntry.message = "Empfangen wurde der Name: " + name;
         logEntry.persist();
 
-        return greeting;
+        try {
+            Greeting greeting = new Greeting();
+            greeting.name = name;
+            greeting.persist();
+            return greeting;
+
+        } catch (Exception e) {
+            throw new RuntimeException("""
+                    // hier wird eine ConstraintViolation nicht gefangen, weil die erst beim commit entsteht!\n"
+                               """);
+
+        }
+    }
+
+    @ServerExceptionMapper
+    public Response toResponse(org.hibernate.exception.ConstraintViolationException exception) {
+
+        return Response.status(Response.Status.CONFLICT)
+
+                .entity("""
+                        {
+                            "ursache": "Name ist bereits bekannt und damit nicht erneut einlesbar.",
+                            "name": "%s",
+                            "sql":"%s"
+                        }
+                        """
+                        .formatted(scoped, exception.getSQL()))
+                .type(MediaType.APPLICATION_JSON)
+                .build();
     }
 }
--- transactions/src/test/java/quarkitecture/GreetingResourceTest.java
+++ transactions/src/test/java/quarkitecture/GreetingResourceTest.java
@@ -1,19 +1,24 @@
 package quarkitecture;
 
 import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.equalTo;
+
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import quarkitecture.logentry.LogEntry;
 
 /**
  * transactions
  */
 @QuarkusTest
 class GreetingResourceTest {
+
     @Test
     void testHelloEndpoint() {
         given()
@@ -25,17 +30,36 @@ class GreetingResourceTest {
 
     @Test
     void testPostHelloEndpoint() {
-        given()
-                .body("""
-                               { "name": "Quarkus" }
-                        """)
+
+        String meinName = "Q-" + UUID.randomUUID().toString().split("-")[1];
+        given().body(
+                """
+                             { "name": "%s" }
+                        """.formatted(meinName))
                 .header("Content-Type", "application/json")
                 .when().post("/hello")
                 .then()
                 .statusCode(201)
                 .contentType(ContentType.JSON)
-                .body("name", equalTo("Quarkus"));
+                .body("name", equalTo(meinName));
 
-                
+        assertThat(LogEntry.find("message like ?1", "%" + meinName).count()).isEqualTo(1L);
+
+        // denselben Namen erneut erfassen produziert einen costraint violation
+        given().body(
+                """
+                        { "name": "%s" }
+                        """.formatted(meinName))
+                .header("Content-Type", "application/json")
+                .when().post("/hello")
+                .then()
+                .statusCode(409) // 409 Conflict
+                .log().all().body("name", equalTo(meinName));
+
+        assertThat(LogEntry.find("message like ?1", "%" + meinName).count())
+                .isEqualTo(1L)
+                .withFailMessage("weil Log und Greeting table in einer XA Transaktion geschrieben werden," +
+                        " darf beim Fehler kein Log vorhanden sein!");
+
     }
 }
