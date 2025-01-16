0bca368ad20d96d917cd98564d80990568fab0c5
avogt-sundn <avogt@s-und-n.de>
2025-01-16 15:59:49 +0000

    quarkus rest client replaces jboss

--- transactions/src/test/java/quarkitecture/booking/ordering/OrderResourceTest.java
+++ transactions/src/test/java/quarkitecture/booking/ordering/OrderResourceTest.java
@@ -2,10 +2,12 @@ package quarkitecture.booking.ordering;
 
 import static io.restassured.RestAssured.given;
 import static java.util.concurrent.TimeUnit.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.awaitility.Awaitility.*;
 
 import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -14,8 +16,8 @@ import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.quarkus.logging.Log;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
-import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.inject.Inject;
@@ -23,6 +25,7 @@ import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.sse.SseEventSource;
+import quarkitecture.booking.domain.Hotel;
 import quarkitecture.booking.domain.Reservation;
 import quarkitecture.booking.domain.TestConstants;
 
@@ -33,11 +36,12 @@ class OrderResourceTest {
     @Inject
     private ObjectMapper objectMapper;
 
-    @TestHTTPResource
+    // @TestHTTPResource
     URI serverSentEvents_GET_URL;
 
     @Test
-    void newOrder() {
+    void newOrder() throws URISyntaxException {
+        serverSentEvents_GET_URL = new URI("http://localhost:8080/booking-requests");
 
         given().when().accept(ContentType.JSON)
                 .contentType(ContentType.JSON)
@@ -49,19 +53,27 @@ class OrderResourceTest {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target(serverSentEvents_GET_URL);
         List<Reservation> received = new CopyOnWriteArrayList<>();
+        List<Throwable> exc = new CopyOnWriteArrayList<>();
+
         SseEventSource source = SseEventSource.target(target).build();
         source.register(inboundSseEvent -> {
             try {
+                Log.fatal("inbound!!...");
+                Reservation value = objectMapper.readValue(inboundSseEvent.readData(), Reservation.class);
+                assertThat(value).isInstanceOf(Hotel.class);
+                Log.fatal(" --> " + value);
+
                 received
-                        .add(objectMapper.readValue(inboundSseEvent.readData(), Reservation.class));
+                        .add(value);
+
             } catch (JsonProcessingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                exc.add(e);
             }
         });
         source.open();
 
-        await().atMost(500, MILLISECONDS).until(() -> received.size() > 0);
+        await().atMost(2500, MILLISECONDS).until(
+                () -> received.size() > 0);
 
     }
 
