80e9b52448d4e7f38a35b98bcace0f3535f29850
avogt-sundn <avogt@s-und-n.de>
2025-01-17 08:32:17 +0000

    clean

--- transactions/src/test/java/quarkitecture/booking/domain/DomainObjectsTest.java
+++ transactions/src/test/java/quarkitecture/booking/domain/DomainObjectsTest.java
@@ -10,7 +10,6 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
