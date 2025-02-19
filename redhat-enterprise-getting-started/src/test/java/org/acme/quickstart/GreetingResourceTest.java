dd23a0ae749d58d52f9d0d6944c7a8a70ad10126
avogt-sundn <avogt@s-und-n.de>
2025-02-19 11:11:39 +0000

    format

--- redhat-enterprise-getting-started/src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ redhat-enterprise-getting-started/src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -1,10 +1,11 @@
 package org.acme.quickstart;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -12,10 +13,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from RESTEasy Reactive"));
     }
 
 }
