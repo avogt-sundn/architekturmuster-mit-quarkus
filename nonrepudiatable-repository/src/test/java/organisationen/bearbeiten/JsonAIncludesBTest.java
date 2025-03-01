d670d7cbea72f4303ae293324880c7829ff888b1
avogt-sundn <avogt@s-und-n.de>
2025-03-01 13:33:19 +0000

    jackson objectmapper von Quarkus kann Dates

--- nonrepudiatable-repository/src/test/java/organisationen/bearbeiten/JsonAIncludesBTest.java
+++ nonrepudiatable-repository/src/test/java/organisationen/bearbeiten/JsonAIncludesBTest.java
@@ -9,9 +9,12 @@ import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
 import organisationen.suchen.modell.Organisation;
 
 /**
@@ -23,39 +26,74 @@ import organisationen.suchen.modell.Organisation;
  * </ul>
  *
  */
+@QuarkusTest
 class JsonAIncludesBTest {
 
-    private final static String jsonArbeitsversion;
-    // private final static String jsonOrganisation;
-    private final static ObjectMapper mapper = new ObjectMapper();
     private final static Organisation organisation;
     private final static Arbeitsversion arbeitsversion;
 
     static {
 
-        mapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
-        mapper.registerModule(new com.fasterxml.jackson.datatype.jdk8.Jdk8Module());
         organisation = TestHelperOrganisation.create();
         organisation.setId(new Random().nextLong());
         arbeitsversion = Arbeitsversion.builder().organisation(organisation).id(
                 new Random().nextLong()).build();
 
-        // jsonOrganisation = jsonb.toJson(organisation);
-        try {
-            jsonArbeitsversion = mapper.writeValueAsString(arbeitsversion);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Test
-    void Jayway_JsonPath_1() throws JsonProcessingException {
+    void Jayway_JsonPath_1_KeinFramework() throws JsonProcessingException {
 
+        final ObjectMapper mapper = new ObjectMapper();
+        /**
+         * Konfiguration des mappers ist hier notwendig, weil Jackson direkt genutzt
+         * wird.
+         */
+        mapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
+        mapper.registerModule(new com.fasterxml.jackson.datatype.jdk8.Jdk8Module());
+
+        final String jsonArbeitsversion = mapper.writeValueAsString(arbeitsversion);
         DocumentContext documentContext = JsonPath.parse(jsonArbeitsversion);
         // selektiere auf das property 'organisation'
         Map<String, Object> jsonpathCreatorName = documentContext.read("$.organisation");
         String json = mapper.writeValueAsString(jsonpathCreatorName);
         Organisation fromJson = mapper.readValue(json, Organisation.class);
+        assertThat(fromJson).isEqualTo(organisation);
+    }
+
+    @Test
+    void JackonFailsOnDateField() throws JsonProcessingException {
+
+        final ObjectMapper mapper = new ObjectMapper();
+        /**
+         * es fehlt die Konfiguration des mappers, weil Jackson direkt genutzt wird.
+         */
+        Exception expected = null;
+        try {
+            mapper.writeValueAsString(arbeitsversion);
+        } catch (InvalidDefinitionException e) {
+            expected = e;
+        }
+        assertThat(expected).isNotNull();
+    }
+
+    @Inject
+    ObjectMapper injectedObjectMapper;
+
+    @Test
+    void Jayway_JsonPath_1() throws JsonProcessingException {
+
+        /**
+         * der jackson objectmapper wird von Quarkus initialisiert! Darum kann er mit
+         * Datumsformat umgehen.
+         */
+        final String jsonArbeitsversion = injectedObjectMapper.writeValueAsString(arbeitsversion);
+
+        DocumentContext documentContext = JsonPath.parse(jsonArbeitsversion);
+        // selektiere auf das property 'organisation'
+        Map<String, Object> jsonpathCreatorName = documentContext.read("$.organisation");
+        String json = injectedObjectMapper.writeValueAsString(jsonpathCreatorName);
+        Organisation fromJson = injectedObjectMapper.readValue(json, Organisation.class);
         assertThat(fromJson).isEqualTo(organisation);
     }
 }
